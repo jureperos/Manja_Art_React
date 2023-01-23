18d5e786e472e91766e0a07f8cad077d90a7ffd0
jureperos <jure.peros@gmail.com>
2023-01-23 14:48:56 +0100

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer-content'>
+            
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import './header.css';
+
+function Header() {
+    return (
+        <div className='header-content'>
+            
+
+        </div>
+    );
+}
+
+export default Header
