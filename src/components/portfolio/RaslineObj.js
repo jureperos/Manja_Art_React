948ab46340a31eb526595a8beb3ae6a7e03230f2
jureperos <jure.peros@gmail.com>
2023-02-13 23:45:14 +0100

    added lazy load images

--- src/components/portfolio/RaslineObj.js
+++ src/components/portfolio/RaslineObj.js
@@ -1,45 +1,84 @@
-const Rastline = {
-    IIllyrica1: require('./assets/rastline/1_Iris illyrica.jpg'),
-    IIllyrica2: require('./assets/rastline/2 Iris illyrica.jpg'),
-    IIllyrica3: require('./assets/rastline/3 Iris illyrica.jpg'),
-    IIllyrica4: require('./assets/rastline/4 Iris illyrica.jpg'),
-    AAltisima: require('./assets/rastline/Ailanthus_altissima_sken.jpg'),
-    ADonax: require('./assets/rastline/Arundo_donax_sken.jpg'),
-    ASquamatus: require('./assets/rastline/Aster_squamatus_sken.jpg'),
-    ATripolium: require('./assets/rastline/Aster_tripolium_sken.jpg'),
-    APortulacoides: require('./assets/rastline/Atriplex_portulacoides_sken.jpg'),
-    CSpicatum: require('./assets/rastline/Centaurium_spicatum_sken.jpg'),
-    FAngustifolia: require('./assets/rastline/Fraxinus_angustifolia_sken.jpg'),
-    GBilobaA: require('./assets/rastline/Ginkgo_biloba_akvarel.png'),
-    GBilobaP: require('./assets/rastline/Ginkgo_biloba_penink.png'),
-    ICrithmoides: require('./assets/rastline/Inula_crithmoides_sken.jpg'),
-    IIllyricaP: require('./assets/rastline/Iris illyrica_penink.jpg'),
-    JMaritimus: require('./assets/rastline/Juncus_maritimus_sken.jpg'),
-    LAngustifolium: require('./assets/rastline/Limonium_angustifolium_sken.png'),
-    LMaritimum: require('./assets/rastline/Linum_maritimum_sken.jpg'),
-    OEuropea: require('./assets/rastline/Olea_europaea.jpg'),
-    PAustralis: require('./assets/rastline/Phragmites_australis_sken.jpg'),
-    IIllyr: require('./assets/rastline/1_Iris illyrica.jpg'),
-    IIllyr: require('./assets/rastline/2 Iris illyrica.jpg'),
-    IIllyr: require('./assets/rastline/3 Iris illyrica.jpg'),
-    IIllyr: require('./assets/rastline/4 Iris illyrica.jpg'),
-    AAlti: require('./assets/rastline/Ailanthus_altissima_sken.jpg'),
-    AD: require('./assets/rastline/Arundo_donax_sken.jpg'),
-    ASquam: require('./assets/rastline/Aster_squamatus_sken.jpg'),
-    ATripo: require('./assets/rastline/Aster_tripolium_sken.jpg'),
-    APortulaco: require('./assets/rastline/Atriplex_portulacoides_sken.jpg'),
-    CSpic: require('./assets/rastline/Centaurium_spicatum_sken.jpg'),
-    FAngustif: require('./assets/rastline/Fraxinus_angustifolia_sken.jpg'),
-    GBil: require('./assets/rastline/Ginkgo_biloba_akvarel.png'),
-    GBil: require('./assets/rastline/Ginkgo_biloba_penink.png'),
-    ICrithmo: require('./assets/rastline/Inula_crithmoides_sken.jpg'),
-    IIllyr: require('./assets/rastline/Iris illyrica_penink.jpg'),
-    JMarit: require('./assets/rastline/Juncus_maritimus_sken.jpg'),
-    LAngustifo: require('./assets/rastline/Limonium_angustifolium_sken.png'),
-    LMarit: require('./assets/rastline/Linum_maritimum_sken.jpg'),
-    OEur: require('./assets/rastline/Olea_europaea.jpg'),
-    PAustr: require('./assets/rastline/Phragmites_australis_sken.jpg')
-}
+// const Rastline = {
+//     IIllyrica1: require('./assets/rastline/1_Iris illyrica.jpg'),
+//     IIllyrica2: require('./assets/rastline/2 Iris illyrica.jpg'),
+//     IIllyrica3: require('./assets/rastline/3 Iris illyrica.jpg'),
+//     IIllyrica4: require('./assets/rastline/4 Iris illyrica.jpg'),
+//     AAltisima: require('./assets/rastline/Ailanthus_altissima_sken.jpg'),
+//     ADonax: require('./assets/rastline/Arundo_donax_sken.jpg'),
+//     ASquamatus: require('./assets/rastline/Aster_squamatus_sken.jpg'),
+//     ATripolium: require('./assets/rastline/Aster_tripolium_sken.jpg'),
+//     APortulacoides: require('./assets/rastline/Atriplex_portulacoides_sken.jpg'),
+//     CSpicatum: require('./assets/rastline/Centaurium_spicatum_sken.jpg'),
+//     FAngustifolia: require('./assets/rastline/Fraxinus_angustifolia_sken.jpg'),
+//     GBilobaA: require('./assets/rastline/Ginkgo_biloba_akvarel.png'),
+//     GBilobaP: require('./assets/rastline/Ginkgo_biloba_penink.png'),
+//     ICrithmoides: require('./assets/rastline/Inula_crithmoides_sken.jpg'),
+//     IIllyricaP: require('./assets/rastline/Iris illyrica_penink.jpg'),
+//     JMaritimus: require('./assets/rastline/Juncus_maritimus_sken.jpg'),
+//     LAngustifolium: require('./assets/rastline/Limonium_angustifolium_sken.png'),
+//     LMaritimum: require('./assets/rastline/Linum_maritimum_sken.jpg'),
+//     OEuropea: require('./assets/rastline/Olea_europaea.jpg'),
+//     PAustralis: require('./assets/rastline/Phragmites_australis_sken.jpg'),
+// }
 
-export default Rastline
+const RastlineArr = [
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
+    {
+        path: require('./assets/rastline/1_Iris illyrica.jpg'),
+        text: 'baug moj dragi dejansko dela!!!'
+    },
 
+]
+
+
+export default RastlineArr
--- src/components/portfolio/RastlinePublic.js
+++ src/components/portfolio/RastlinePublic.js
@@ -1,19 +1,43 @@
-import Rastline from "./RaslineObj"
-import { LazyLoadImage } from "react-lazy-load-image-component"
-import './RastlinePublic.css'
+import RastlineArr from "./RaslineObj";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import './RastlinePublic.css';
+import { useState } from 'react';
+
 
 
 function RastlinePublic () {
+    const [ImgOpen, setImgOpen] = useState(false)
+
+    function whatState() {
+        setImgOpen(!ImgOpen)
+    }
+
+        //finds which image is being clicked so the lightbox opens on that image
+    function lightBoxActivate (path) {
+        if (ImgOpen === false) {
+            const imgPath = path
+            return (
+                
+            )
+        } else return console.log('imgopentrue')
+    }
+
     return (
-        <div className="plant-img">
-            {Object.entries(Rastline).map(([key, value]) => {
+        //using a map method to iterate and access the RastlineArr array objects
+        <div>
+            {RastlineArr.map((rastline) => {
                 return (
+                    <div onClick={() => {lightBoxActivate(rastline.path); whatState();}} >
                         <LazyLoadImage 
-                            height={400}
-                            alt={ key }
-                            src={ value }
-                            offset={ 20 }
+                            height={350}
+                            alt={ 'nika zaenkrat' }
+                            src={ rastline.path }
+                            offset={ 100 }
+                            effect='blur'
                         />
+                        <p>{rastline.text}</p>
+                    </div>
                 )
             })}
         </div>
