790f456870bc3b8dd5baec295485b0d5997b216e
jureperos <jure.peros@gmail.com>
2023-02-28 07:12:52 +0100

    initial blog1 commit

---
+++ src/components/Blog.js
@@ -0,0 +1,13 @@
+import './blog.css'
+
+function Blog() {
+    return (
+        <>
+            <img></img>
+            <h1>To je moj naslov</h1>
+            <div></div>
+        </>
+    )
+}
+
+export default Blog
