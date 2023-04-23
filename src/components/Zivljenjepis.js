8f35dd997d4a1134125aed0d400e9907f0fa2551
jureperos <jure.peros@gmail.com>
2023-04-23 20:23:17 +0200

    dodal naročilo KPG za 2023

--- src/components/Zivljenjepis.js
+++ src/components/Zivljenjepis.js
@@ -1,55 +1,57 @@
 import './Zivljenjepis.css'
 
-function Zivljenjepis () {
+function Zivljenjepis() {
     return (
         <>
-                <h1 className='cv-title'>UMETNIŠKE IZKUŠNJE</h1>
-        <ul className='cv-list'>
-            <li>2022: Naročilo Krajinskega parka Goričko akvarelnih ilustracij gozdnih vrst ptic za opremljanje informativnih tabel</li>
+            <h1 className='cv-title'>UMETNIŠKE IZKUŠNJE</h1>
+            <ul className='cv-list'>
+                <li>2023: Naročilo Krajinskega parka Goričko akvarelnih ilustracij različnih živalskih vrst</li>
 
-            <li>2022: Ilustracija orjaške črepahe (<i>Chelonia mydas</i>), ki je objavljena v znanstvenem članku Apparent increasing importance
-            of Adriatic Sea as a developmental habitat for Mediterranean green sea turtles (<i>Chelonia mydas</i>) (Jančič et. al 2022)</li>
+                <li>2022: Naročilo Krajinskega parka Goričko akvarelnih ilustracij gozdnih vrst ptic za opremljanje informativnih tabel</li>
 
-            <li>2022: Razstava bioloških ilustracij rastlinskih in živalskih vrst na sredozemskem slanem travniku v Medgeneracijskem
-            središču Ankaran in občini Ankaran</li>
+                <li>2022: Ilustracija orjaške črepahe (<i>Chelonia mydas</i>), ki je objavljena v znanstvenem članku Apparent increasing importance
+                    of Adriatic Sea as a developmental habitat for Mediterranean green sea turtles (<i>Chelonia mydas</i>) (Jančič et. al 2022)</li>
 
-            <li>2022: Naročilo občine Ankaran akvarelnih ilustracij rastlinskih in živalskih vrst, za opremljanje informativnih tabel na
-            območju naravne vrednote obrežno močvirje pri Sv. Nikolaju (sredozemski slani travnik)</li>
+                <li>2022: Razstava bioloških ilustracij rastlinskih in živalskih vrst na sredozemskem slanem travniku v Medgeneracijskem
+                    središču Ankaran in občini Ankaran</li>
 
-            <li>2022: Objava v glasilu <a href='https://clan.lovska-zveza.si/userfiles/Lovec/pdf/Lovec-2021-12.pdf' target={'_blank'}>Lovec</a></li>
+                <li>2022: Naročilo občine Ankaran akvarelnih ilustracij rastlinskih in živalskih vrst, za opremljanje informativnih tabel na
+                    območju naravne vrednote obrežno močvirje pri Sv. Nikolaju (sredozemski slani travnik)</li>
 
-            <li>2021: Objava v <a href='https://issuu.com/centermladihkoper/docs/cmk_55-2' target={'_blank'}>CMK magazinu</a></li>
+                <li>2022: Objava v glasilu <a href='https://clan.lovska-zveza.si/userfiles/Lovec/pdf/Lovec-2021-12.pdf' target={'_blank'}>Lovec</a></li>
 
-            <li>2017: Umetniška akvarelna razstava “Življenjski cikel ilirske perunike”, v sklopu moje zaključne naloge</li>
-        </ul>
+                <li>2021: Objava v <a href='https://issuu.com/centermladihkoper/docs/cmk_55-2' target={'_blank'}>CMK magazinu</a></li>
 
-        <h1 className='cv-title'>AKADEMSKE IZKUŠNJE</h1>
-        <ul className='cv-list'>
-            <li>2022: Objava znanstvenega članka Diversity and Typology of Land-Use Explain the Occurrence of Alien Plants in a
-            Protected Area (Glasnović et. al 2022)</li>
+                <li>2017: Umetniška akvarelna razstava “Življenjski cikel ilirske perunike”, v sklopu moje zaključne naloge</li>
+            </ul>
 
-            <li>2019: Laboratorijsko delo na populacijski genetski strukturi lisice (<i>Vulpes vulpes</i>) v Sloveniji (magistrsko delo). Ime
-            projekta: Divjad v naseljih, na cestah in drugih nelovnih površinah: težave, izzivi, rešitve</li>
+            <h1 className='cv-title'>AKADEMSKE IZKUŠNJE</h1>
+            <ul className='cv-list'>
+                <li>2022: Objava znanstvenega članka Diversity and Typology of Land-Use Explain the Occurrence of Alien Plants in a
+                    Protected Area (Glasnović et. al 2022)</li>
 
-            <li>2019: Prispevek k poznavanju ekologije in upravljanju z izbranimi endemičnimi ali zavarovanimi vrstami nevretenčarjev v
-            porečju reke Pivke (ŠIPK projekt) na vrsti strašničin mravljiščar (<i>Phengaris teleius</i>)</li>
+                <li>2019: Laboratorijsko delo na populacijski genetski strukturi lisice (<i>Vulpes vulpes</i>) v Sloveniji (magistrsko delo). Ime
+                    projekta: Divjad v naseljih, na cestah in drugih nelovnih površinah: težave, izzivi, rešitve</li>
 
-            <li>2018: Projekt LIKE – Živeti na Kraškem robu, kjer sem pomagala pri terenskem delu (popis rastišč raznolistne mačine
-            (<i>Serratula lycopifolia</i>)) ter se naravovarstveno udejstvovala znotraj planinskega in plezalskega društva</li>
+                <li>2019: Prispevek k poznavanju ekologije in upravljanju z izbranimi endemičnimi ali zavarovanimi vrstami nevretenčarjev v
+                    porečju reke Pivke (ŠIPK projekt) na vrsti strašničin mravljiščar (<i>Phengaris teleius</i>)</li>
 
-            <li>2018: Projekt Kras.Re.Vita, kjer sem pomagala pri vzpostavitvi monitoringa ogrožene travniške morske čebulice (<i>Scilla litardierei</i>) v Krajinskem parku Planinsko polje</li>
+                <li>2018: Projekt LIKE – Živeti na Kraškem robu, kjer sem pomagala pri terenskem delu (popis rastišč raznolistne mačine
+                    (<i>Serratula lycopifolia</i>)) ter se naravovarstveno udejstvovala znotraj planinskega in plezalskega društva</li>
 
-            <li>2017: Zasnova ekonomsko-vzdržnega poslovnega modela za Botanični vrt Sežana (PKP projekt)</li>
+                <li>2018: Projekt Kras.Re.Vita, kjer sem pomagala pri vzpostavitvi monitoringa ogrožene travniške morske čebulice (<i>Scilla litardierei</i>) v Krajinskem parku Planinsko polje</li>
 
-            <li>2017: Diplomirana varstvena biologinja</li>
-        </ul>
+                <li>2017: Zasnova ekonomsko-vzdržnega poslovnega modela za Botanični vrt Sežana (PKP projekt)</li>
 
-        <h1 className='cv-title'>OSTALE IZKUŠNJE</h1>
-        <ul className='cv-list'>
-            <li>Osnovno znanje v programih Adobe Photoshop, Adobe Indesign in Adobe Illustrator</li>
+                <li>2017: Diplomirana varstvena biologinja</li>
+            </ul>
 
-            <li>Osnovno znanje programa ArcGIS in QGIS</li>
-        </ul>
+            <h1 className='cv-title'>OSTALE IZKUŠNJE</h1>
+            <ul className='cv-list'>
+                <li>Osnovno znanje v programih Adobe Photoshop, Adobe Indesign in Adobe Illustrator</li>
+
+                <li>Osnovno znanje programa ArcGIS in QGIS</li>
+            </ul>
         </>
     )
 }
