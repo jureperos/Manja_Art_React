d5e4ae68e7129125194ec9c9cea41c0ca32e60d2
jureperos <jure.peros@gmail.com>
2023-05-17 10:09:58 +0200

    added blog_2

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MyHelmet from './components/MyHelmet';
+import ScrollToTop from 'react-scroll-to-top';
+import Content from './components/Content';
+
+function App() {
+
+  return (
+      <div className='app'>
+        <MyHelmet />
+        <ScrollToTop smooth='True' id='scroll-top'/>
+        <Content />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/blog/blog_2/Blog_2.js
@@ -0,0 +1,22 @@
+function blog_2() {
+    return (
+        <div className='blog_2-ctn'>
+            <div className='blog_1-intro'>
+                <h1 className="blog_1-title">Kako pripraviti paleto</h1>
+                <div className="blog_1-datum">Torek, 16.5.2023</div>
+            </div>
+
+                
+            <div className='txt-1'>
+                <p>Barve v paleti oziroma set lahko kupimo že vnaprej, lahko pa kupimo paleto in dodamo barve, ki jih mi želimo. Plastične palete so izredno poceni in to katero bom uporabila v tem blogu sem kupila v trgovini Pri levčku. Paleta je stala le 5.2 € in skupno ima 28 prostorčkov za barvo. Ta paleta mi bo služila za botanične ilustracije, kar se tudi odraža pri izbranih barvah.</p>
+
+            </div>
+            
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+    )
+} 
