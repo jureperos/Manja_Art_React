73e608eb8c174472593e7613a1e06ca7d09a95e3
jureperos <jure.peros@gmail.com>
2023-06-11 22:23:11 +0200

    added MergedArr

---
+++ src/components/portfolio/MergedArr.js
@@ -0,0 +1,9 @@
+import RastlineArr from "./RaslineArr";
+import ZivaliArr from "./ZivaliArr";
+import PortretiArr from "./PortretiArr";
+import LinorezArr from "./LinorezArr";
+import OstaloArr from "./OstaloArr";
+
+const MergedArr = [...RastlineArr, ...ZivaliArr, ...PortretiArr, ...LinorezArr, ...OstaloArr];
+
+export default MergedArr
