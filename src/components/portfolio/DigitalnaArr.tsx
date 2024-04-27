675751b968c12473b90d3477802e0136d79d65f4
Jure <jure.peros@gmail.com>
2024-04-27 12:32:18 +0200

    new images

--- src/components/portfolio/DigitalnaArr.tsx
+++ src/components/portfolio/DigitalnaArr.tsx
@@ -1,67 +1,94 @@
 import { ImageAttributes } from "./arrayInterface"
 
 const DigitalnaArr: Array<ImageAttributes> = [
-    {
-        src: '/assets/digital/monarh_gosenica.jpg',
-        title: 'Gosenica monarha',
-        sciName: 'Danaus plexippus',
-        description: 'Digitalna ilustracija',
-    },
-    {
-        src: '/assets/digital/monarh_naveji.jpg',
-        title: 'Monarh metulj',
-        sciName: 'Danaus plexippus',
-        description: 'Digitalna ilustracija',
-    },
-    {
-        src: '/assets/digital/krog_pupek.jpeg',
-        title: 'Življenski krog repatih dvoživk',
-        description: 'Digitalna ilustracija',
-    },
+  {
+    src: '/assets/digital/tiranozaver.png',
+    title: 'Tiranozaver',
+    sciName: 'Tiranozaver rex',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/Karbonski_gozd.jpg',
+    title: 'Karbonski gozd',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/Gozd.png',
+    title: 'Gozd',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/Merjasec.PNG',
+    title: 'Divja svinja',
+    sciName: 'Sus scrofa',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/Pomlad_.jpg',
+    title: 'Pomlad',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/monarh_gosenica.jpg',
+    title: 'Gosenica monarha',
+    sciName: 'Danaus plexippus',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/monarh_naveji.jpg',
+    title: 'Monarh metulj',
+    sciName: 'Danaus plexippus',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/krog_pupek.jpeg',
+    title: 'Življenski krog repatih dvoživk',
+    description: 'Digitalna ilustracija',
+  },
 
-    {
-        src: '/assets/digital/krog_zaba.jpeg',
-        title: 'Življenski krog brezrepih dvoživk',
-        description: 'Digitalna ilustracija',
-    },
-    {
-        src: '/assets/digital/monarh.jpeg',
-        title: 'Monarh',
-        sciName: 'Danaus plexippus',
-        description: 'Digitalna ilustracija ',
-    },
+  {
+    src: '/assets/digital/krog_zaba.jpeg',
+    title: 'Življenski krog brezrepih dvoživk',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/monarh.jpeg',
+    title: 'Monarh',
+    sciName: 'Danaus plexippus',
+    description: 'Digitalna ilustracija ',
+  },
 
-    {
-        src: '/assets/digital/tascica.jpeg',
-        title: 'Taščica',
-        sciName: 'Erithacus rubecula',
-        description: 'Digitalna ilustracija',
-    },
-    {
-        src: '/assets/digital/oljka_digital.jpeg',
-        title: 'Oljka',
-        description: 'Digitalna ilustracija',
-    },
-    {
-        src: '/assets/digital/borovnice.jpeg',
-        title: 'Borovnica',
-        description: 'Digitalna ilustracija',
-    },
-    {
-        src: '/assets/digital/lilija.jpeg',
-        title: 'Lilija',
-        description: 'Digitalna ilustracija',
-    },
-    {
-        src: '/assets/digital/jabolko.jpeg',
-        title: 'Jabolko in mleko',
-        description: 'Digitalna ilustracija ',
-    },
-    {
-        src: '/assets/digital/kresnicke.jpeg',
-        title: 'Kresničke',
-        description: 'Digitalna ilustracija ',
-    },
+  {
+    src: '/assets/digital/tascica.jpeg',
+    title: 'Taščica',
+    sciName: 'Erithacus rubecula',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/oljka_digital.jpeg',
+    title: 'Oljka',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/borovnice.jpeg',
+    title: 'Borovnica',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/lilija.jpeg',
+    title: 'Lilija',
+    description: 'Digitalna ilustracija',
+  },
+  {
+    src: '/assets/digital/jabolko.jpeg',
+    title: 'Jabolko in mleko',
+    description: 'Digitalna ilustracija ',
+  },
+  {
+    src: '/assets/digital/kresnicke.jpeg',
+    title: 'Kresničke',
+    description: 'Digitalna ilustracija ',
+  },
 ]
 
 export default DigitalnaArr
